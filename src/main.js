48a46b282250950b2298cc2d369f4b4c02d7714c
FelixXG <felixxg@foxmail.com>
2019-05-29 22:34:09 +0800

    bable和vue.config配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import Antd from "ant-design-vue"
+// import 'ant-design-vue/dist/antd.less'
+import { Button } from "ant-design-vue";
+// Vue.use(Antd)
+Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false;
 
