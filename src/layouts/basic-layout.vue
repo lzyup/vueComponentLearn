c36072221c06ea3a7633728fe74f8fa954a602ed
FelixXG <felixxg@foxmail.com>
2019-05-30 23:18:49 +0800

    项目vue-router的基础配置

---
+++ src/layouts/basic-layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header />
+    <footer />
+    <slider-menu></slider-menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from "./header";
+import footer from "./footer";
+import sliderMenu from "./slider-menu";
+export default {
+  components: {
+    /* eslint-disable vue/no-unused-components */
+    header,
+    footer,
+    sliderMenu
+  }
+};
+</script>
+
+<style></style>
