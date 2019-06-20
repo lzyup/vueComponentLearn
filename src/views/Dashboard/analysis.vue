fb493260be0e830224e3ec26fc99ae65866622a4
FelixXG <felixxg@foxmail.com>
2019-06-20 21:56:47 +0800

    asycn await

--- src/views/Dashboard/analysis.vue
+++ src/views/Dashboard/analysis.vue
@@ -3,7 +3,32 @@
 </template>
 
 <script>
-export default {};
+export default {
+  created() {
+    console.log("script start");
+    setTimeout(() => {
+      console.log("settimeout");
+    }, 0);
+    this.async1();
+    new Promise(resolve => {
+      console.log("promise1");
+      resolve();
+    }).then(function() {
+      console.log("promise2");
+    });
+    console.log("script end");
+  },
+  methods: {
+    async async1() {
+      console.log("async1 start");
+      await this.async2();
+      console.log("async1 end");
+    },
+    async async2() {
+      console.log("async2");
+    }
+  }
+};
 </script>
 
 <style></style>
