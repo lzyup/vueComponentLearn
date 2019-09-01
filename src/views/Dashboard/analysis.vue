af661c401a826887d318342ebdccad8cc3b7c817
Luzy <felixxg@foxmail.com>
2019-09-02 00:01:08 +0800

    chart组件&mock数据

--- src/views/Dashboard/analysis.vue
+++ src/views/Dashboard/analysis.vue
@@ -4,7 +4,7 @@
 
 <script>
 import Chart from "../../components/chart";
-import axios from "axios";
+import request from "../../utils/request";
 import { clearInterval } from "timers";
 export default {
   data() {
@@ -27,27 +27,29 @@ export default {
   },
   methods: {
     getChartData() {
-      axios
-        .get("/api/dashboard/chart", { params: { ID: 12345 } })
-        .then(response => {
-          this.chartOption = {
-            title: {
-              text: "ECharts 入门示例"
-            },
-            tooltip: {},
-            xAxis: {
-              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-            },
-            yAxis: {},
-            series: [
-              {
-                name: "销量",
-                type: "bar",
-                data: response.data
-              }
-            ]
-          };
-        });
+      request({
+        url: "/api/dashboard/chart",
+        method: "get",
+        params: { ID: 12345 }
+      }).then(response => {
+        this.chartOption = {
+          title: {
+            text: "ECharts 入门示例"
+          },
+          tooltip: {},
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: response.data
+            }
+          ]
+        };
+      });
     }
   }
 };
